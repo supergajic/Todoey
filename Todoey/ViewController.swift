88c8f75fb5818cfe275e83fb4a2a7dee51667c2f
Milan Gajic <supergajic@gmail.com>
2019-01-10 11:55:57 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Development on 2019-01-10.
+//  Copyright © 2019 dropmedia.se. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
